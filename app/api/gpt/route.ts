1261ac233e51c1baccdc992291574e475986ee1d
eric_wang <jadeface.scholar@gmail.com>
2023-09-18 23:41:01 +0800

    fix: fix ci error

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -6,7 +6,7 @@ const apiKey = process.env.OPENAI_API_KEY || ''
 const baseUrl = ((process.env.OPENAI_API_BASE_URL) || 'https://api.openai.com').trim().replace(/\/$/, '')
 const model = process.env.MODEL || 'gpt-3.5-turbo'
 const api = new ChatGPTAPI({
-    apiKey,
+    apiKey: apiKey,
     apiBaseUrl: `${baseUrl}/v1`,
     completionParams: {
         model: model,
